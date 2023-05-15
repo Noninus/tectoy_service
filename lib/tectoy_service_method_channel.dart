506d444b2297cbabd57bd7a208eb58f3a81ed708
noninus <rafinha.nonino7@gmail.com>
2023-05-15 14:01:32 -0300

    import 'dart:typed_data' show Uint8List;

--- lib/tectoy_service_method_channel.dart
+++ lib/tectoy_service_method_channel.dart
@@ -4,6 +4,7 @@ import 'package:image/image.dart' as img;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:typed_data' show Uint8List;
 
 import 'tectoy_service_platform_interface.dart';
 
